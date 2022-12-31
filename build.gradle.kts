b4bb8fc489fe704e66f2d02e30c9b9ce9008af57
Zxilly <zhouxinyu1001@gmail.com>
2023-01-01 01:40:31 +0800

    build: bump gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "7.3.1" apply false
+    id("com.android.library") version "7.4.0-rc03" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
 
